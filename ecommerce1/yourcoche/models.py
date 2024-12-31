d5179058c7d08a55972f1b6ef3220ae1720c5639
Theo Wlasiczuk Ballesteros <124530613+theowla@users.noreply.github.com>
2024-12-31 14:56:05 +0000

    database configured and contact advances

--- ecommerce1/yourcoche/models.py
+++ ecommerce1/yourcoche/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Car(models.Model):
+    # Fields for the Car model
+
+    # Auto-incremented ID field is created automatically by Django
+    make = models.CharField(max_length=100)  # Make of the car (e.g., Toyota, Ford)
+    model = models.CharField(max_length=100)  # Model of the car (e.g., Corolla, Mustang)
+    price = models.DecimalField(max_digits=10, decimal_places=2)  # Price of the car
+    pictures = models.ImageField(upload_to='car_pictures/', null=True, blank=True)  # Picture of the car
+    description = models.TextField()  # Description of the car
+    STATE_CHOICES = [  # Choices for the state of the car
+        ('available', 'Available'),
+        ('sold', 'Sold'),
+        ('reserved', 'Reserved'),
+    ]
+    state = models.CharField(max_length=10, choices=STATE_CHOICES, default='available')  # State of the car
+
+    # String representation for the admin panel and shell
+    def __str__(self):
+        return f"{self.make} {self.model} - {self.state}"
