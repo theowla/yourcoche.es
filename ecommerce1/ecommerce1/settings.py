14bf14f9f3980117fb22377401d7ad4c6b02052c
Theo Wlasiczuk Ballesteros <124530613+theowla@users.noreply.github.com>
2025-01-03 17:12:49 +0000

    major changes, implemented posts and search.

--- ecommerce1/ecommerce1/settings.py
+++ ecommerce1/ecommerce1/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -116,9 +117,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Optional: Use if a global static folder exists
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'images_logos')  # Folder to store uploaded files
--- ecommerce1/yourcoche/views.py
+++ ecommerce1/yourcoche/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render, HttpResponse
+from .models import Car
+from django.db.models import Q  # Import Q for complex queries
 
 # Create your views here.
 def home(request):
-    return render(request, "index.html")
+    cars = Car.objects.all()
+    # Pass the cars to the template
+    return render(request, 'index.html', {'cars': cars})
 
 def contacto(request):
     return render(request, "contacto.html")
+
+def search_cars(request):
+    query = request.GET.get('query', '').strip()  # Get the search query and remove extra spaces
+    cars = None  # Initialize cars to None in case no query is provided
+
+    if query:  # Check if the query is not empty
+        cars = Car.objects.filter(
+            Q(make__icontains=query) | Q(model__icontains=query)
+        )
+    
+    return render(request, 'post.html', {'cars': cars, 'query': query})
