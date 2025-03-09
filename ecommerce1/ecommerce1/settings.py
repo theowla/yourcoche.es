9570d885aa921f8f4f93a6f784e0ce96dc17718d
Theo Wlasiczuk Ballesteros <124530613+theowla@users.noreply.github.com>
2025-03-09 11:46:45 +0000

    static

--- ecommerce1/ecommerce1/settings.py
+++ ecommerce1/ecommerce1/settings.py
@@ -118,9 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_URL = '/static/'  # URL for static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Development static files location
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Collected static files for production
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
